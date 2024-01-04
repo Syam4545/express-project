6c2c38511a0dac209d627011937f3072f403425a
chaithra0604 <chaithru2002@gmail.com>
2024-01-04 10:26:18 +0530

    contact controller

---
+++ controllers/contactControllers.js
@@ -0,0 +1,19 @@
+const getContact = (req, res) => {
+    res.status(200).json({ message: 'get all contacts' })
+}
+const postContact = (req, res) => {
+    res.status(200).json({ message: `create contacts ${req.body.name}` })
+}
+
+const updateContact = (req, res) => {
+    res.status(200).json({ message: `update contacts for ${req.params.id}` })
+}
+
+
+const deleteContact = (req, res) => {
+    res.status(200).json({ message: `delete contacts for ${req.params.id}` })
+}
+
+
+
+module.exports = { getContact,postContact,updateContact,deleteContact }
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const dotenv=require('dotenv').config()
-const contactrouter=require('./routes/contactRoutes')
+const contactRouter=require('./routes/contactRoutes')
 const app=express()
+app.use(express.json());
 const port=process.env.PORT||3000
 
-app.use('/api/contacts',contactrouter)
+app.use('/api/contacts',contactRouter)
 
 
 app.listen(port,()=>{
